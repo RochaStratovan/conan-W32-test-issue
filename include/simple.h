7f8d91ff1edf455fd38a3e897dc3770bb1797765
John Rocha <Rocha@Stratovan.com>
2024-01-30 09:20:20 -0800

    Add the simple logic

--- include/simple.h
+++ include/simple.h
@@ -1,14 +1,3 @@
 #pragma once
 
-#include <vector>
-#include <string>
-
-
-#ifdef _WIN32
-  #define SIMPLE_EXPORT __declspec(dllexport)
-#else
-  #define SIMPLE_EXPORT
-#endif
-
-SIMPLE_EXPORT void simple();
-SIMPLE_EXPORT void simple_print_vector(const std::vector<std::string> &strings);
+void simple(void);
