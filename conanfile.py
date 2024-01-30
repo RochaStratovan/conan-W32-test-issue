ec52e87ef99104bccb7cfbd2b95011886e3b2d1f
John Rocha <Rocha@Stratovan.com>
2024-01-30 09:20:01 -0800

    conanfile.py - Strip down to just what we need

--- conanfile.py
+++ conanfile.py
@@ -7,28 +7,14 @@ class simpleRecipe(ConanFile):
     version = "1.0"
     package_type = "library"
 
-    # Optional metadata
-    license = "<Put the package license here>"
-    author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of simple package here>"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
-
     # Binary configuration
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
+    options = {"shared": [False], "fPIC": [True]}
     default_options = {"shared": False, "fPIC": True}
 
-    # Sources are located in the same place as this recipe, copy them to the recipe
+    # Sources are located in the same place as this recipe, copy them to the
+    # recipe
     exports_sources = "CMakeLists.txt", "src/*", "include/*"
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            self.options.rm_safe("fPIC")
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
 
     def layout(self):
         cmake_layout(self)
@@ -50,8 +36,3 @@ class simpleRecipe(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = ["simple"]
-
-    
-
-    
-
